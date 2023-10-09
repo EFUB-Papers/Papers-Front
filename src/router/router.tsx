fa8a81b53fd5ce804c71b5c3b05371acdbc4a11b
Jeongmin Cho <jojm0829@gmail.com>
2023-10-09 19:29:14 +0900

    ✨ feat: 유저카드 생성

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,4 @@
-import ScrapCard from 'components/_common/ScrapCard/ScrapCard';
+import UserCard from 'components/_common/UserCard/UserCard';
 import { createBrowserRouter } from 'react-router-dom';
 import NavbarLayout from '../layout/navbarLayout';
 import MyPage from '../pages/myPage';
@@ -7,16 +7,12 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: (
-      <ScrapCard
+      <UserCard
         width={280}
-        scrapId={0}
-        link="https://biz.heraldcorp.com/view.php?ud=20230912000401"
-        linkTitle="풍요의 제주 ‘일멍쉬멍’ 워케이션 성지로"
-        imgurl="https://res.heraldm.com/content/image/2023/09/12/20230912000420_0.jpg"
-        title="풍요의 제주 ‘일멍쉬멍’ 워케이션 성지로"
-        content="아침이 자랑처럼 마리아 까닭입니다. 별빛이 잠, 보고, 시인의 봅니다. 같이 이름을 이름자를 하아침이 자랑처럼 마리아 까닭입니다. 별빛이 잠, 보고, 시인의 봅니다. 같이 이름을 이름자를 하"
-        heartCnt={12}
-        author="나는 강아지다"
+        userId={0}
+        imgurl="https://www.perfocal.com/blog/content/images/size/w960/2021/01/Perfocal_17-11-2019_TYWFAQ_100_standard-3.jpg"
+        nickname="Jamie0829"
+        introduction="이것은 "
       />
     )
   },
