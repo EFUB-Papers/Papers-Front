25f97dabbe410dee7dbe2113fcad6c6dff9334f9
Jeongmin Cho <jojm0829@gmail.com>
2023-10-09 18:32:53 +0900

    ✨ feat: 스크랩카드 생성

---
+++ src/components/_common/ScrapCard/style.ts
@@ -0,0 +1,107 @@
+import { Link } from 'react-router-dom';
+import { boxShadow, flexCenter, omitText } from 'style/common';
+import { styled } from 'styled-components';
+
+const Wrapper = styled.div<{ $width: number }>`
+  width: ${({ $width }) => $width + 'px'};
+  display: flex;
+  flex-direction: column;
+`;
+
+const LinkBox = styled(Link)`
+  width: 100%;
+  aspect-ratio: 10 / 3;
+  border-radius: 15px;
+  padding: 10px 15px;
+  font-size: 17px;
+  ${boxShadow}
+  ${flexCenter}
+  &:hover {
+    cursor: pointer;
+  }
+  overflow: hidden;
+`;
+
+const LinkTitle = styled.div`
+  object-fit: cover;
+  display: block;
+  width: 100%;
+  /* 2줄 말줄임표 */
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+`;
+
+const ScrapBox = styled.div`
+  width: 100%;
+  aspect-ratio: 1 / 1.15;
+  border-radius: 15px;
+  padding: 15px;
+  ${boxShadow}
+  display:flex;
+  flex-direction: column;
+  &:hover {
+    cursor: pointer;
+  }
+  overflow: hidden;
+`;
+
+const ImageBox = styled.div`
+  width: 100%;
+  aspect-ratio: 5 / 3;
+  ${flexCenter}
+  overflow: hidden;
+`;
+
+const Img = styled.img`
+  width: 100%;
+`;
+
+const Title = styled.div`
+  font-size: 18px;
+  margin-top: 8px;
+  ${omitText}
+`;
+
+const Content = styled.div`
+  font-size: 14px;
+  margin-top: 6px;
+  /* 3줄 말줄임표 */
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+`;
+
+const Footer = styled.div`
+  margin-top: auto;
+  display: flex;
+  align-items: center;
+  ${omitText}
+`;
+
+const HeartCnt = styled.div`
+  margin: 0px 4px;
+`;
+
+const Author = styled.div`
+  margin-left: auto;
+  color: ${({ theme }) => theme.COLOR.darkGrey};
+`;
+
+export const S = {
+  Wrapper,
+  LinkBox,
+  ScrapBox,
+  ImageBox,
+  Img,
+  Title,
+  Content,
+  Footer,
+  HeartCnt,
+  Author,
+  LinkTitle
+};
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,25 @@
-import CategoryBar from 'components/_common/CategoryBar/CategoryBar';
+import ScrapCard from 'components/_common/ScrapCard/ScrapCard';
 import { createBrowserRouter } from 'react-router-dom';
 import NavbarLayout from '../layout/navbarLayout';
 import MyPage from '../pages/myPage';
 
 const router = createBrowserRouter([
-  { element: <CategoryBar />, path: '/' },
+  {
+    path: '/',
+    element: (
+      <ScrapCard
+        width={280}
+        scrapId={0}
+        link="https://biz.heraldcorp.com/view.php?ud=20230912000401"
+        linkTitle="풍요의 제주 ‘일멍쉬멍’ 워케이션 성지로"
+        imgurl="https://res.heraldm.com/content/image/2023/09/12/20230912000420_0.jpg"
+        title="풍요의 제주 ‘일멍쉬멍’ 워케이션 성지로"
+        content="아침이 자랑처럼 마리아 까닭입니다. 별빛이 잠, 보고, 시인의 봅니다. 같이 이름을 이름자를 하아침이 자랑처럼 마리아 까닭입니다. 별빛이 잠, 보고, 시인의 봅니다. 같이 이름을 이름자를 하"
+        heartCnt={12}
+        author="나는 강아지다"
+      />
+    )
+  },
   {
     element: <NavbarLayout />,
     children: [
