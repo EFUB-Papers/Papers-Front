607a4e5e9e9897ff4a001026924e0d04ae63e817
Jeongmin Cho <jojm0829@gmail.com>
2023-10-22 16:27:24 +0900

    🔧 fix: 풀 받은 뒤 에러해결
    import문 경로의 폴더명 대소문자 에러 해결

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
-import NavbarLayout from 'layout/NavbarLayout/NavbarLayout';
-import FolderPage from 'pages/FolderPage/FolderPage';
+import NavbarLayout from 'layout/navbarLayout/NavbarLayout';
+import FolderPage from 'pages/folderPage/FolderPage';
 import CategoryPage from '../pages/CategoryPage/CategoryPage';
-import HeaderLayout from 'layout/HeaderLayout/HeaderLayout';
+import HeaderLayout from 'layout/headerLayout/HeaderLayout';
 import MainPage from 'pages/MainPage/MainPage';
 const router = createBrowserRouter([
   {
