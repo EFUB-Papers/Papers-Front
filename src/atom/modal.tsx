f848c17d1fe6d7085547c03d24679c419f35622b
ooherin <555ohr@naver.com>
2023-11-21 05:51:39 +0900

    🔧 fix : 필요 없는 코드 삭제₩

--- src/atom/modal.tsx
+++ src/atom/modal.tsx
@@ -1,32 +1,5 @@
 import { atom } from 'recoil';
 
-// export const folderEditModal = atom({
-//   key: 'isFolderEditModalOpen',
-//   default: false
-// });
-//
-// export const folderSelectModal = atom({
-//   key: 'isFolderSelectModalOpen',
-//   default: false
-// });
-
-// type Edit = {
-//   option: 'edit';
-//   folderList: OneFolderType[];
-// };
-//
-// type Select = {
-//   option: 'select';
-//   scrapId: number;
-//   folderList: OneFolderType[];
-// };
-//
-// type Add = {
-//   option: 'add';
-// };
-//
-// export type FolderType = Edit | Select | Add;
-
 export const folderModalAtom = atom({
   key: 'folderModal',
   default: {
