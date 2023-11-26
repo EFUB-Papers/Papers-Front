ee96ec5aeaee7a681f9275e018ea83ca59503911
ooherin <555ohr@naver.com>
2023-11-26 16:23:17 +0900

    💄 ui : 다크 모드 아이콘 추가

--- src/components/FolderPage/MyMenu/MyMenu.tsx
+++ src/components/FolderPage/MyMenu/MyMenu.tsx
@@ -2,11 +2,18 @@ import { ReactComponent as FolderIcon } from 'asset/navBar/folder.svg';
 import { ReactComponent as FollowingIcon } from 'asset/navBar/following.svg';
 import { ReactComponent as HeartIcon } from 'asset/navBar/heart.svg';
 import { ReactComponent as LogoutIcon } from 'asset/navBar/logout.svg';
+import { ReactComponent as FolderIconWhite } from 'asset/navBar/folderWhite.svg';
+import { ReactComponent as FollowingIconWhite } from 'asset/navBar/followingWhite.svg';
+import { ReactComponent as HeartIconWhite } from 'asset/navBar/heartWhite.svg';
+import { ReactComponent as LogoutIconWhite } from 'asset/navBar/logoutWhite.svg';
 import { M } from './style';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { modeState } from '../../../atom/mode';
 
 const MyMenu = () => {
   const navigate = useNavigate();
+  const mode = useRecoilValue(modeState);
 
   const onClickLogout = () => {
     localStorage.removeItem('papersToken');
@@ -19,28 +26,28 @@ const MyMenu = () => {
       <M.MenuWrapper>
         <M.OneMenuWrapper onClick={() => navigate(`/folder/%{nickname}`)}>
           <M.MenuIcon>
-            <FolderIcon />
+            {mode == 'light' ? <FolderIcon /> : <FolderIconWhite />}
           </M.MenuIcon>
           <M.MenuText>내 폴더</M.MenuText>
         </M.OneMenuWrapper>
 
         <M.OneMenuWrapper onClick={() => navigate('/like')}>
           <M.MenuIcon>
-            <FollowingIcon />
+            {mode == 'light' ? <FollowingIcon /> : <FollowingIconWhite />}
           </M.MenuIcon>
           <M.MenuText>좋아요한 스크랩</M.MenuText>
         </M.OneMenuWrapper>
 
         <M.OneMenuWrapper onClick={() => navigate('/following')}>
           <M.MenuIcon>
-            <HeartIcon />
+            {mode == 'light' ? <HeartIcon /> : <HeartIconWhite />}
           </M.MenuIcon>
           <M.MenuText>팔로잉 목록</M.MenuText>
         </M.OneMenuWrapper>
 
         <M.OneMenuWrapper>
           <M.MenuIcon>
-            <LogoutIcon />
+            {mode == 'light' ? <LogoutIcon /> : <LogoutIconWhite />}
           </M.MenuIcon>
           <M.MenuText onClick={onClickLogout}>로그아웃</M.MenuText>
         </M.OneMenuWrapper>
